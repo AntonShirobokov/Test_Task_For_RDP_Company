0f8e77a015e398be296c891c60eff4b65ae65a37
AntonShirobokov <shirobokov.anton2016@gmail.com>
2026-03-21 10:56:31 +0400

    Init commit

---
+++ src/main/java/com/shirobokov/inventoryreservationservice/InventoryReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shirobokov.inventoryreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryReservationServiceApplication.class, args);
+    }
+
+}
